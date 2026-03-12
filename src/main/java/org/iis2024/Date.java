9156a61f18d91154323f21670d47691d6bd327e9
ppduru07 -crypto <0611682643@uma.es>
2026-03-12 10:15:26 +0100

    Hecho

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -20,7 +20,7 @@ public class Date {
   private final int day;
   private final int month;
   private final int year;
-
+  
   public void methodAddedByOwner()
   {
     System.out.println("Method added by the owner") ;
@@ -61,10 +61,11 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  // Constructor modified by the owner
+  public Date(int dia, int mes, int año) {
+    this.day = dia;
+    this.month = mes;
+    this.year = año;
   }
 
   /**
